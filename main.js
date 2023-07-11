957770bc96fbd6f16ec9a3a52b7300ba1f9d2cd4
reshmi-02 <reshmihash123@gmail.com>
2023-07-11 22:09:51 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,859 @@
+console.log("Arithmatic operators");
+console.log("1.Addition");
+console.log("a.value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=a+b;
+console.log("c=a+b="+c);
+
+
+console.log("b.value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="10";
+console.log("b="+b);
+c=a+b;
+console.log("c=a+b="+c);
+
+
+console.log("c.string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a+b;
+console.log("c=a+b="+c);
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmi";
+console.log("b="+b);
+c=a+b;
+console.log("c=a+b="+c);
+
+
+
+console.log("2.sub");
+console.log("a.value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=a-b;
+console.log("c=a-b="+c);
+
+
+console.log("b.value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a-b;
+console.log("c=a-b="+c);
+
+
+console.log("c.string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a-b;
+console.log("c=a-b="+c);
+
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmi";
+console.log("b="+b);
+c=a-b;
+console.log("c=a-b="+c);
+
+
+
+
+
+console.log("3.multiple");
+console.log("a.value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=a*b;
+console.log("c=a*b="+c);
+
+
+console.log("b.value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a*b;
+console.log("c=a*b="+c);
+
+
+console.log("c.string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a*b;
+console.log("c=a*b="+c);
+
+
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmi";
+console.log("b="+b);
+c=a*b;
+console.log("c=a*b="+c);
+
+
+
+console.log("4.div ");
+console.log("a.value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=3;
+console.log("b="+b);
+c=a/b;
+console.log("c=a/b="+c);
+
+
+console.log("b.value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a/b;
+console.log("c=a/b="+c);
+
+
+console.log("c.string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a/b;
+console.log("c=a/b="+c);
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmi";
+console.log("b="+b);
+c=a/b;
+console.log("c=a/b="+c);
+
+
+
+console.log("4.modulo");
+console.log("a.value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=3;
+console.log("b="+b);
+c=a%b;
+console.log("c=a%b="+c);
+
+
+console.log("b.value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a%b;
+console.log("c=a%b="+c);
+
+
+console.log("c.string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a%b;
+console.log("c=a%b="+c);
+
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmi";
+console.log("b="+b);
+c=a%b;
+console.log("c=a%b="+c);
+
+
+console.log("4)exponential")
+console.log("a.value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=3;
+console.log("b="+b);
+c=a**b;
+console.log("c=a**b="+c);
+
+
+console.log("b.value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a**b;
+console.log("c=a**b="+c);
+
+
+console.log("c.string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a**b;
+console.log("c=a**b="+c);
+
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmi";
+console.log("b="+b);
+c=a**b;
+console.log("c=a**b="+c);
+
+
+
+
+
+console.log("comparision operator");
+console.log("1)greater than");
+console.log("a)value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=8;
+console.log("b="+b);
+c=a>b;
+console.log("c=a>b="+c);
+
+var a,b;
+a=5;
+console.log("a="+a);
+b=8;
+console.log("b="+b);
+c=a>b;
+console.log("c=a>b="+c);
+
+
+console.log("b)value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a>b;
+console.log("c=a>b="+c);
+
+
+console.log("c)string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a>b;
+console.log("c=a>b="+c);
+
+
+var a,b;
+a="reshmi";
+console.log("a="+a);
+b="reshmihashini";
+console.log("b="+b);
+c=b>a;
+console.log("c=a>b="+c);
+
+
+
+
+console.log("2)less than");
+console.log("a)value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=8;
+console.log("b="+b);
+c=a<b;
+console.log("c=a<b="+c);
+
+var a,b;
+a=5;
+console.log("a="+a);
+b=8;
+console.log("b="+b);
+c=a<b;
+console.log("c=a<b="+c);
+
+
+console.log("b)value,string");
+var a,b;
+a=1034567;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a<b;
+console.log("c=a<b="+c);
+
+
+console.log("c)string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="142935678900";
+console.log("b="+b);
+c=a<b;
+console.log("c=a<b="+c);
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a<b;
+console.log("c=a<b="+c);
+
+var a,b;
+a="142956789";
+console.log("a="+a);
+b="hashini";
+console.log("b="+b);
+c=a<b;
+console.log("c=a<b="+c);
+
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmihashini";
+console.log("b="+b);
+c=a<b;
+console.log("c=a<b="+c);
+
+
+
+console.log("3)greater than equalto");
+console.log("a)value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=8;
+console.log("b="+b);
+c=a>=b;
+console.log("c=a>=b="+c);
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=a>=b;
+console.log("c=a>=b="+c);
+
+var a,b;
+a=5;
+console.log("a="+a);
+b=8;
+console.log("b="+b);
+c=a>=b;
+console.log("c=a>=b="+c);
+
+
+console.log("b)value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a>=b;
+console.log("c=a>=b="+c);
+
+
+console.log("c)string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a>=b;
+console.log("c=a>=b="+c);
+
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmi";
+console.log("b="+b);
+c=a>=b;
+console.log("c=a>=b="+c);
+
+
+
+
+
+console.log("4)less than equalto");
+console.log("a)value,value");
+var a,b;
+a=5;
+console.log("a="+a);
+b=8;
+console.log("b="+b);
+c=a<=b;
+console.log("c=a<=b="+c);
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=a<=b;
+console.log("c=a<=b="+c);
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=8;
+console.log("b="+b);
+c=a<=b;
+console.log("c=a<=b="+c);
+
+
+console.log("b)value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a<=b;
+console.log("c=a<=b="+c);
+
+
+console.log("c)string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a<=b;
+console.log("c=a<=b="+c);
+
+
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmi";
+console.log("b="+b);
+c=a<=b;
+console.log("c=a<=b="+c);
+
+
+
+console.log("5)equalto");
+console.log("a)value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=8;
+console.log("b="+b);
+c=a==b;
+console.log("c=a==b="+c);
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=a==b;
+console.log("c=a==b="+c);
+
+
+console.log("b)value,string");
+var a,b;
+a=5;
+console.log("a="+a);
+b="5";
+console.log("b="+b);
+c=a==b;
+console.log("c=a==b="+c);
+
+
+console.log("c)value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a==b;
+console.log("c=a==b="+c);
+
+
+console.log("c)string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a==b;
+console.log("c=a==b="+c);
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmi";
+console.log("b="+b);
+c=a==b;
+console.log("c=a==b="+c);
+
+
+console.log("6)triple equalto");
+console.log("a)value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=8;
+console.log("b="+b);
+c=a===b;
+console.log("c=a===b="+c);
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=a===b;
+console.log("c=a===b="+c);
+
+
+console.log("b)value,string");
+var a,b;
+a=5;
+console.log("a="+a);
+b="5";
+console.log("b="+b);
+c=a===b;
+console.log("c=a===b="+c);
+
+
+console.log("c)value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a===b;
+console.log("c=a===b="+c);
+
+
+console.log("c)string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a===b;
+console.log("c=a===b="+c);
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmi";
+console.log("b="+b);
+c=a===b;
+console.log("c=a===b="+c);
+
+
+console.log("7)not equalto");
+console.log("a)value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=8;
+console.log("b="+b);
+c=a!=b;
+console.log("c=a!=b="+c);
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=a!=b;
+console.log("c=a!=b="+c);
+
+
+console.log("b)value,string");
+var a,b;
+a=5;
+console.log("a="+a);
+b="5";
+console.log("b="+b);
+c=a!=b;
+console.log("c=a!=b="+c);
+
+
+console.log("c)value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a!=b;
+console.log("c=a!=b="+c);
+
+
+console.log("c)string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a!=b;
+console.log("c=a!=b="+c);
+
+
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmi";
+console.log("b="+b);
+c=a!=b;
+console.log("c=a!=b="+c);
+
+
+console.log("8)not double equalto");
+console.log("a)value,value");
+var a,b;
+a=10;
+console.log("a="+a);
+b=8;
+console.log("b="+b);
+c=a!==b;
+console.log("c=a!==b="+c);
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=a!==b;
+console.log("c=a!==b="+c);
+
+
+console.log("b)value,string");
+var a,b;
+a=5;
+console.log("a="+a);
+b="5";
+console.log("b="+b);
+c=a!==b;
+console.log("c=a!==b="+c);
+
+
+console.log("c)value,string");
+var a,b;
+a=10;
+console.log("a="+a);
+b="resh";
+console.log("b="+b);
+c=a!==b;
+console.log("c=a!==b="+c);
+
+
+console.log("c)string,string");
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="1429";
+console.log("b="+b);
+c=a!==b;
+console.log("c=a!==b="+c);
+
+
+
+var a,b;
+a="hashini";
+console.log("a="+a);
+b="reshmi";
+console.log("b="+b);
+c=a!==b;
+console.log("c=a!==b="+c);
+
+
+
+
+console.log("logical operator");
+console.log("1)AND");
+console.log("a)value");
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=8;
+console.log("c="+c);
+d=(a==b)&&(b>c)&&(c>b);
+console.log("d=(a==b)&&(b>c)&&(c>b)="+d);
+
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=8;
+console.log("c="+c);
+d=(a+b)&&(b-c)&&(c*b);
+console.log("d=(a+b)&&(b-c)&&(c*b)="+d);
+
+
+console.log("b)value,namestring");
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c="resh";
+console.log("c="+c);
+d=(a==b)&&(b>c)&&(c>b);
+console.log("d=(a==b)&&(b>c)&&(c>b)="+d);
+
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c="resh";
+console.log("c="+c);
+d=(a+b)&&(b-c)&&(c*b);
+console.log("d=(a+b)&&(b-c)&&(c*b)="+d);
+
+console.log("c)value,valuestring");
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c="5";
+console.log("c="+c);
+d=(a==b)&&(b>c)&&(c<b);
+console.log("d=(a==b)&&(b>c)&&(c<b)="+d);
+
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c="5";
+console.log("c="+c);
+d=(a+b)&&(b-c)&&(c*b);
+console.log("d=(a+b)&&(b-c)&&(c*b)="+d);
+
+
+
+
+
+console.log("logical operator");
+console.log("1)OR");
+console.log("a)value");
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=8;
+console.log("c="+c);
+d=(a==b)||(b>c)||(c>b);
+console.log("d=(a==b)||(b>c)||(c>b)="+d);
+
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c=8;
+console.log("c="+c);
+d=(a+b)||(b-c)||(c*b);
+console.log("d=(a+b)||(b-c)||(c*b)="+d);
+
+
+console.log("b)value,namestring");
+var a,b;
+a="resh";
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c="resh";
+console.log("c="+c);
+d=(a==b)||(b>c)||(c>b);
+console.log("d=(a==b)||(b>c)||(c>b)="+d);
+
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c="resh";
+console.log("c="+c);
+d=(a+b)||(b-c)||(c*b);
+console.log("d=(a+b)||(b-c)||(c*b)="+d);
+
+console.log("c)value,valuestring");
+var a,b;
+a="10";
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c="5";
+console.log("c="+c);
+d=(a==b)||(b>c)||(c<b);
+console.log("d=(a==b)||(b>c)||(c<b)="+d);
+
+
+var a,b;
+a=10;
+console.log("a="+a);
+b=10;
+console.log("b="+b);
+c="5";
+console.log("c="+c);
+d=(a+b)||(b-c)||(c*b);
+console.log("d=(a+b)||(b-c)||(c*b)"+d);
